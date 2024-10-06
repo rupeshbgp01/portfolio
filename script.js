91465fc7a1652ce4fa350c11845cdca8e30b5394
Rupesh Kumar <104664321+rupeshbgp01@users.noreply.github.com>
2024-10-06 09:04:59 +0530

    Update scroll behaviour

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+var loader = document.getElementById("preloader");
+    window.addEventListener("load", function () {
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 500);
+    });
+
 
   var swiper = new Swiper(".mySwiper", {
+    loop: true,
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: "auto",
-    fade:true,
+    fade: true,
     coverflowEffect: {
       rotate: 50,
       stretch: 0,
@@ -12,7 +20,36 @@
       modifier: 1,
       slideShadows: true,
     },
+    keyboard: {
+      enabled: true,
+    },
+    speed: 800,
     pagination: {
       el: ".swiper-pagination",
     },
-  });
+});
+
+
+
+let isScrolling = false; 
+
+
+document.addEventListener('wheel', (e) => {
+  e.preventDefault();
+
+  if (isScrolling) return;
+  isScrolling = true;
+
+
+  if (e.deltaY > 0) {
+
+    swiper.slideNext();
+  } else {
+
+    swiper.slidePrev();
+  }
+
+  setTimeout(() => {
+    isScrolling = false;
+  }, 400);
+});
